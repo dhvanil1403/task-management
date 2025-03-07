7207ca041064a60a2e2a696618272e41f5a94c84
dhvanil1403 <pd14030304@gmail.com>
2025-03-07 22:02:15 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 
-router.get('/', taskController.getAllTasks);
+// router.get('/', taskController.getAllTasks);
 router.get('/create', taskController.getCreateTask);
-router.post('/', taskController.createTask);
+// router.post('/', taskController.createTask);
 router.post('/:id/update', taskController.updateTask);
 router.post('/:id/delete', taskController.deleteTask);
 
