7207f6fd75bac50331180c8388f528ff4c81ae91
dhvanil1403 <pd14030304@gmail.com>
2025-03-07 23:25:50 +0530

    Update manager.js

--- routes/manager.js
+++ routes/manager.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
 const managerController = require('../controllers/managerController');
 
 // Manager dashboard
